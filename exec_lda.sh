9450021f83fd0dc7e9bf7f635b2e34cb3c2b1dab
rdorado <ruben.dorados@gmail.com>
2015-10-13 13:10:32 -0400

    Fix exec_lda

--- exec_lda.sh
+++ exec_lda.sh
@@ -1,5 +1,5 @@
 
-WORK_DIRECTORY=$HOME"../data/twentynews_lda"
+WORK_DIRECTORY=$HOME"/data/twentynews_lda"
 
 for i in {4..20}
 do
@@ -9,5 +9,6 @@ do
     ./lda-c-dist/topics.py "$WORK_DIRECTORY/lda_data_$i/final.beta" "$WORK_DIRECTORY/lda_data.vocab" $j > $WORK_DIRECTORY"/lda_data_"$i"_"$j".keywords" 
   done
 done
+
 
 
